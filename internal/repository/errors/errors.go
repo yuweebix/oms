ebbefec0801e7948d078f51a0abb49efc8b31243
yuweebix <yuweebix@gmail.com>
2024-06-20 02:32:28 +0300

    [GOHW-3]: micro

--- internal/repository/errors/errors.go
+++ internal/repository/errors/errors.go
@@ -3,6 +3,6 @@ package errors
 import "errors"
 
 var (
-	ErrNoRowsAffected      = errors.New("no rows affected")
-	ErrTooManyRowsAffected = errors.New("too many rows affected")
+	ErrNoOrdersAffected      = errors.New("no orders affected")
+	ErrTooManyOrdersAffected = errors.New("too many orders affected")
 )
