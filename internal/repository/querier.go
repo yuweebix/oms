d2e2c9dac7c7d27208e4417653abe423dbd90752
yuweebix <yuweebix@gmail.com>
2024-06-28 14:57:03 +0300

    [GOHW-5]: add comments to querier

--- internal/repository/querier.go
+++ internal/repository/querier.go
@@ -8,12 +8,14 @@ import (
 	"gitlab.ozon.dev/yuweebix/homework-1/internal/models"
 )
 
+// Querier интерфейс, исполняющий sql запросы. В качестве возможных куриеров можно использовать пул или же например транзакцию.
 type Querier interface {
 	Exec(ctx context.Context, sql string, arguments ...interface{}) (pgconn.CommandTag, error)
 	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
 }
 
+// GetQuerier проверяет, была ли создана транзакция и возвращает её. В противном случае возвращается пул.
 func (r *Repository) GetQuerier(ctx context.Context) Querier {
 	if tx, ok := ctx.Value(txKey).(Querier); ok && tx != nil {
 		return tx
@@ -25,6 +27,7 @@ type txKeyType string
 
 const txKey txKeyType = "tx"
 
+// RunTx используется для начала транзакции с предоставленными опциями
 func (r *Repository) RunTx(ctx context.Context, opts models.TxOptions, fn func(ctxTX context.Context) error) error {
 	pgxOpts := convertTxOptions(opts)
 	tx, err := r.pool.BeginTx(ctx, pgxOpts)
